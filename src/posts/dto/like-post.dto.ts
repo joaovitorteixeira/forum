9f9d75ef305832749aaf6c6bbd2ce0acf00bfefa
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2022-07-07 21:28:30 +1000

    refactor: validate if post exists

--- src/posts/dto/like-post.dto.ts
+++ src/posts/dto/like-post.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { ExistElement } from '../../Util/Decorator/exist-element.decorator';
+import Post from '../entity/posts.entity';
 
 export default class LikePostDto {
   @ApiProperty({
     description: 'The id of the post to be liked',
     example: 1,
+  })
+  @ExistElement(Post, {
+    message: 'The post does not exist',
   })
   id: number;
 }
