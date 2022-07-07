4c78ec6fc006dd3f536c2b25447cf9b2ac22ad36
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2022-07-07 19:47:12 +1000

    feat: add post like user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import Post from './posts/entity/posts.entity';
 import { PostsModule } from './posts/posts.module';
+import PostLikesUser from './posts/entity/post-likes-user.entity';
 
 @Module({
   imports: [
@@ -24,7 +25,7 @@ import { PostsModule } from './posts/posts.module';
         username: configService.get('MYSQL_USER'),
         password: configService.get('MYSQL_PASSWORD'),
         database: configService.get('MYSQL_DATABASE'),
-        entities: [User, Post],
+        entities: [User, Post, PostLikesUser],
         synchronize: configService.get('SYNCHRONIZE_DATABASE'),
       }),
     }),
