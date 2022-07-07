7f4e0fe133ca4c33f0559ec51348b7ff5f07010d
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2022-07-07 18:16:13 +1000

    feat: add create post method

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import User from 'src/users/users.entity';
+import CreatePostDto from './dto/create-post.dto';
+import Post from './posts.entity';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor() {}
+
+  /**
+   * Create a new post on the database
+   * @param post New post to be created
+   * @param user The user who created the post
+   * @returns The created post
+   */
+  async create(post: CreatePostDto, user: User): Promise<Post> {
+    const newPost = new Post();
+
+    newPost.title = post.title;
+    newPost.content = post.content;
+    newPost.user = user;
+
+    return newPost.save();
+  }
+}
