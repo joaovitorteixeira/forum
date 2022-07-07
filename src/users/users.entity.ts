35c748dcc1cdb83e84881ee703abf2f27f208e9f
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2022-07-07 16:25:35 +1000

    refactor: add first and last name

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -14,7 +14,10 @@ export default class User extends BaseEntity {
   id: number;
 
   @Column()
-  name: string;
+  firstName: string;
+
+  @Column()
+  lastName: string;
 
   @Column({
     unique: true,
