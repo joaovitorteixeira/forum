df4b912991b46c4da5a3c18942c45cccc46af666
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2022-07-07 19:47:26 +1000

    feat: add method to like post

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,6 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import User from 'src/users/entity/users.entity';
 import CreatePostDto from './dto/create-post.dto';
+import PostLikesUser from './entity/post-likes-user.entity';
 import Post from './entity/posts.entity';
 
 @Injectable()
@@ -21,5 +26,30 @@ export class PostsService {
     newPost.user = user;
 
     return newPost.save();
+  }
+
+  /**
+   *  Try to like a post
+   * @param postId The id of the post to be liked
+   * @param user The user who liked the post
+   * @returns If post exists and doesn't already have the user liked it, it will be liked
+   */
+  async like(postId: number, user: User): Promise<PostLikesUser> {
+    const post = await Post.createQueryBuilder('post')
+      .leftJoinAndSelect('post.likes', 'likes')
+      .where('post.id = :id', { id: postId })
+      .getOne();
+
+    if (!post) throw new NotFoundException('Post not found');
+
+    if (post.likes.find((like) => like.id === user.id))
+      throw new ForbiddenException('User already liked this post');
+
+    const newLike = new PostLikesUser();
+
+    newLike.userId = user.id;
+    newLike.postId = post.id;
+
+    return newLike.save();
   }
 }
