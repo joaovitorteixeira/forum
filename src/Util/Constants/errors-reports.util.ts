eac7c29bb842844067fcfb245ee9ace836175a32
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2022-07-08 11:34:28 +1000

    feat: add min and max report error

--- src/Util/Constants/errors-reports.util.ts
+++ src/Util/Constants/errors-reports.util.ts
@@ -1,9 +1,27 @@
 import { ValidationArguments } from 'class-validator';
 
 const PASSWORD_MISMATCH = 'Passwords do not match.';
+
+const FIELD_MAX_VALUE = (
+  maxValue: number,
+): ((value: ValidationArguments) => string) => {
+  return (value: ValidationArguments): string => {
+    return `${value.value} must be less than ${maxValue}`;
+  };
+};
+
+const FIELD_MIN_VALUE = (
+  minValue: number,
+): ((value: ValidationArguments) => string) => {
+  return (value: ValidationArguments): string => {
+    return `${value.value} must be greater than ${minValue}`;
+  };
+};
+
 const FIELD_REQUIRED = (value: ValidationArguments) => {
   return `${value.property} is required.`;
 };
+
 const FIELD_LENGTH = (
   minValue: number,
   maxValue: number,
@@ -20,4 +38,6 @@ export const REPORT_ERRORS = {
   PASSWORD_MISMATCH,
   FIELD_LENGTH,
   FIELD_REQUIRED,
+  FIELD_MAX_VALUE,
+  FIELD_MIN_VALUE,
 };
