c76b06e69afa452f8aea242d39ab5d64a9244eb3
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2022-07-09 06:46:07 +1000

    feat: add check terms and conditions decorator

--- src/Util/Decorator/get-user.decorator.ts
+++ src/Util/Decorator/get-user.decorator.ts
@@ -1,5 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+/**
+ * Pipe to get the user from the request.
+ */
 const GetUser = createParamDecorator((_, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
   return request.user;
