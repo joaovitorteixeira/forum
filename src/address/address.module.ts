a82584f8fc4e879c0bdc9fc8636d5eaee042106a
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2022-07-10 12:12:41 +1000

    refactor: add address repository

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GoogleMapsApiModule } from '../google-maps-api/google-maps-api.module';
 import { AddressService } from './address.service';
+import Address from './entity/address.entity';
 
 @Module({
   providers: [AddressService],
+  imports: [GoogleMapsApiModule, TypeOrmModule.forFeature([Address])],
 })
 export class AddressModule {}
--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -1,19 +1,18 @@
-import { Client, LatLngLiteral } from '@googlemaps/google-maps-services-js';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { OnEvent } from '@nestjs/event-emitter';
-import UserRegisterDto from '../users/dto/user-register.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { GoogleMapsApiService } from '../google-maps-api/google-maps-api.service';
 import User from '../users/entity/users.entity';
 import CreateAddressDto from './dto/create-address.dto';
 import Address from './entity/address.entity';
 
 @Injectable()
-export class AddressService extends Client {
-  private readonly accessKey = this.config.get('GOOGLE_API_KEY');
-
-  constructor(private config: ConfigService) {
-    super();
-  }
+export class AddressService {
+  constructor(
+    @InjectRepository(Address) private addressRepository: Repository<Address>,
+    private googleMapsApi: GoogleMapsApiService,
+  ) {}
 
   /**
    * Create a new address for the user.
@@ -28,32 +27,13 @@ export class AddressService extends Client {
     address: CreateAddressDto;
     user: User;
   }): Promise<User> {
-    const newAddress = new Address();
-    const { lng, lat } = await this.getCoordinates(address);
+    const { lng, lat } = await this.googleMapsApi.getCoordinates(address);
 
-    newAddress.lng = '' + lng;
-    newAddress.lat = '' + lat;
-    user.address = newAddress;
-
-    return user.save();
-  }
-
-  /**
-   * Get the address from the latitude and longitude.
-   * @param address User's address
-   * @returns Location of the address
-   */
-  public async getCoordinates(
-    address: CreateAddressDto,
-  ): Promise<LatLngLiteral> {
-    const googleRes = await this.geocode({
-      params: {
-        address: `${address.street}, ${address.number}, ${address.city}, ${address.state}, ${address.postalCode}`,
-        key: this.accessKey,
-      },
+    user.address = this.addressRepository.create({
+      lat: '' + lat,
+      lng: '' + lng,
     });
 
-    const { lng, lat } = googleRes.data.results[0].geometry.location;
-    return { lng, lat };
+    return user.save();
   }
 }
