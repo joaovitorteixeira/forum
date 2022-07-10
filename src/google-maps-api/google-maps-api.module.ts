690ea4d62560989c98376f459cfb33096303f98b
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2022-07-10 12:26:33 +1000

    fix: import config module

--- src/google-maps-api/google-maps-api.module.ts
+++ src/google-maps-api/google-maps-api.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GoogleMapsApiService } from './google-maps-api.service';
 
 @Module({
   providers: [GoogleMapsApiService],
+  imports: [ConfigModule],
   exports: [GoogleMapsApiService],
 })
 export class GoogleMapsApiModule {}
